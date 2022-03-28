321cefbfabee7451e9140689c2fa66825b0cbc53
Mikegv <xmik3yvx@live.com>
2022-03-28 15:20:35 -0700

    feat(navbar):added navbar component to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import routes from './config/routes'
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       {routes}
     </div>
   );
