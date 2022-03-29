2609f9d5942b39cedd983f20dc766372b20b5232
Mikegv <xmik3yvx@live.com>
2022-03-29 10:35:44 -0700

    feat(navbar):some base styling in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div class='navbar'>
-            
+            <div>Mikes Portfolio</div>
+            <ul>
+                <li><Link>Mike</Link></li>
+                <li><Link>Villa</Link></li>
+                <li><Link>Jr</Link></li>
+            </ul>
         </div>
     );
 };
