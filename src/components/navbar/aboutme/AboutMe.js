be7c85716236bde8930553a0e86b9daee249cfed
Mikegv <xmik3yvx@live.com>
2022-03-29 15:27:16 -0700

    feat(AboutMe):added AboutMe component and route

---
+++ src/components/navbar/aboutme/AboutMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutMe.css'
+
+const AboutMe = () => {
+    return (
+        <p className='aboutMe'>
+            My name is Mike Villa. I am a web developer who specializes in front end development. My main
+            tools are Javascript, CSS and React.  I am currently working as an React apprentice at Alpha Works in 
+            Fresno California. If I'm not tucked away on the computer writing code or watching videos about programming,
+            I'm probably spending time outside with my family. I also enjoy watching a good movie. 
+            Feel free to contact me or check out my github using the links below. 
+        </p>
+    );
+};
+
+export default AboutMe;
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import AboutMe from '../components/navbar/aboutme/AboutMe';
 
 export default(
     <Switch>
-        <Route exact path='/' />
+        <Route exact path='/' component={AboutMe} />
     </Switch>
 );
