d37447d10bc46ae02fcc90e90e487b4133265641
Mikegv <xmik3yvx@live.com>
2022-03-29 16:08:33 -0700

    feat(ProjectsPage):added projectsPage component and route

---
+++ src/components/projectsPage/ProjectsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProjectsPage.css'
+
+const ProjectsPage = () => {
+    return (
+        <div className='projectsContainer'>
+            <div className='project'>f</div>
+            <div className='project'>f</div>
+            <div className='project'>f</div>
+        </div>
+    );
+};
+
+export default ProjectsPage;
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import AboutMe from '../components/aboutme/AboutMe';
+import ProjectsPage from '../components/projectsPage/ProjectsPage';
 
 export default(
     <Switch>
         <Route exact path='/' component={AboutMe} />
+        <Route path='/projects' component={ProjectsPage} />
     </Switch>
 );
