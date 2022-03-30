3ad19636b1f20ffcba1cef8bc2b558e8f9098559
Mikegv <xmik3yvx@live.com>
2022-03-30 15:08:45 -0700

    edited about me info

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -3,13 +3,15 @@ import './AboutMe.css'
 
 const AboutMe = () => {
     return (
+       
         <p className='aboutMe'>
-            My name is Mike Villa. I am a web developer who specializes in front end development. My main
+            <p className='aboutMeTitle'>My name is <br/>Mike Villa</p>
+            and I am a web developer who specializes in front end development. My main
             tools are Javascript, CSS and React.  I am currently working as an React apprentice at Alpha Works in 
-            Fresno California. If I'm not tucked away on the computer writing code or watching videos about programming,
-            I'm probably spending time outside with my family. I also enjoy watching a good movie. 
+            Fresno California. I am passionate about all things technology but especially love programming. 
             Feel free to contact me or check out my github using the links below. 
         </p>
+        
     );
 };
 
