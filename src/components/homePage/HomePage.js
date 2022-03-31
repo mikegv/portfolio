27e4c7e760a948e005e7a9eb4e2bebd789539df8
Mikegv <xmik3yvx@live.com>
2022-03-31 16:39:29 -0700

    changed homepage

---
+++ src/components/homePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HomePage.css'
+
+const HomePage = () => {
+    return (
+        <div className='homepageContainer'>
+            <div className='mike'>Heyo</div>
+            <div className='homepage'>
+                <p>My name is Mike Villa</p>
+                <p>I am a front end developer </p>
+                <p>I make dynamic websites </p>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
