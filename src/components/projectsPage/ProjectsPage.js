5eaefc0556bf58cceb3bc527e9f950f2d0b1340b
Mikegv <xmik3yvx@live.com>
2022-04-01 15:51:45 -0700

    chore(Footer):footer correctly positioned now

--- src/components/projectsPage/ProjectsPage.js
+++ src/components/projectsPage/ProjectsPage.js
@@ -6,22 +6,58 @@ const ProjectsPage = () => {
         <div className='projectsContainer'>
             <div className='project'>
                 <a href="https://mikegv.github.io/Base-Apparel-Coming-Soon/" target='_blank' >
-                <img src={process.env.PUBLIC_URL + '/images/projects/baseapparel.gif'}            
-                alt='base product' 
-                />
+                    <img src={process.env.PUBLIC_URL + '/images/projects/baseapparel.gif'}
+                        alt='base product'
+                    />
                 </a>
             </div>
             <div className='project'>
                 <a href="https://fourcardsreact.herokuapp.com/" target='_blank'>
-                <img src={process.env.PUBLIC_URL + '/images/projects/fourcards.PNG'} 
-                alt='four cards' 
-                />
+                    <img src={process.env.PUBLIC_URL + '/images/projects/fourcards.PNG'}
+                        alt='four cards'
+                    />
                 </a>
 
-</div>
+            </div>
 
-          
-            
+
+            <div className='project'>
+                <a href="https://mikegv.github.io/Base-Apparel-Coming-Soon/" target='_blank' >
+                    <img src={process.env.PUBLIC_URL + '/images/projects/baseapparel.gif'}
+                        alt='base product'
+                    />
+                </a>
+            </div>
+            <div className='project'>
+                <a href="https://fourcardsreact.herokuapp.com/" target='_blank'>
+                    <img src={process.env.PUBLIC_URL + '/images/projects/fourcards.PNG'}
+                        alt='four cards'
+                    />
+                </a>
+
+            </div>
+
+
+
+
+
+
+            <div className='project'>
+                <a href="https://mikegv.github.io/Base-Apparel-Coming-Soon/" target='_blank' >
+                    <img src={process.env.PUBLIC_URL + '/images/projects/baseapparel.gif'}
+                        alt='base product'
+                    />
+                </a>
+            </div>
+            <div className='project'>
+                <a href="https://fourcardsreact.herokuapp.com/" target='_blank'>
+                    <img src={process.env.PUBLIC_URL + '/images/projects/fourcards.PNG'}
+                        alt='four cards'
+                    />
+                </a>
+
+            </div>
+
         </div>
     );
 };
