f4165e68a1c6dea0377e355c9018f2f098b6a7f6
Mikegv <xmik3yvx@live.com>
2022-04-01 09:44:38 -0700

    animation on home page

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -5,8 +5,7 @@ const AboutMe = () => {
     return (
        
         <p className='aboutMe'>
-            <p className='aboutMeTitle'>My name is <br/>Mike Villa</p>
-            I am a web developer who specializes in front end development. My main
+            Hi, my name is Mike. I am a web developer who specializes in front end development. My main
             tools are Javascript, CSS and React.  I am currently working as an React apprentice at Alpha Works in 
             Fresno California. I am passionate about all things technology but especially love programming. 
             Feel free to contact me or check out my github using the links below. 
--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './HomePage.css'
 
 const HomePage = () => {
+    
     return (
         <div className='homepageContainer'>
-            <div className='mike'>Heyo</div>
             <div className='homepage'>
-                <p>My name is Mike Villa</p>
+                <p class='titleSentence'>My name is Mike Villa</p>
                 <p>I am a front end developer </p>
                 <p>I make dynamic websites </p>
             </div>
