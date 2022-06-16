552b5984e5e33083332f332ab2e8c9cc3d3f93f2
mikegv <xmik3yvx@live.com>
2022-06-16 14:25:14 -0700

    contact form functionality

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,7 +4,7 @@ import useForm from '../../hooks/useForm';
 
 const Contact = () => {
     
-    const { name, message, email, handleNameChange, handleEmailChange, handleMessageChange, submitAttempt, submitHandler } = useForm()
+    const { name, message, email, formRef, handleNameChange, handleEmailChange, handleMessageChange, submitAttempt, submitHandler } = useForm()
 
     return (
         <
@@ -14,13 +14,13 @@ const Contact = () => {
         exit={{scaleY: 0}}
         transition={{duration: .5}}
         >
-        <form onSubmit={submitHandler} className='contactForm'>
+        <form onSubmit={submitHandler} ref={formRef} className='contactForm'>
             <label>Your Name:</label>
-            <input type='text' placeholder='Your name...' name='name' onChange={handleNameChange} value={name} />
+            <input type='text' placeholder='Your name...' name='name' onChange={handleNameChange} value={name} required/>
             <label>Your Email:</label>
-            <input type='text' placeholder='Your Email...' name='email' onChange={handleEmailChange} value={email} />
+            <input type='email' placeholder='Your Email...' name='email' onChange={handleEmailChange} value={email} required />
             <label>Your Message</label>
-            <textarea type='text' placeholder='Your message...' name='message' onChange={handleMessageChange} value={message} />
+            <textarea type='text' placeholder='Your message...' name='message' onChange={handleMessageChange} value={message} required/>
             
             {submitAttempt && <p>Form is not completed.</p>}
             
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import emailjs from 'emailjs-com'
+const SERVICE_ID = process.env.REACT_APP_MY_SERVICE_ID
+const TEMPLATE_ID = process.env.REACT_APP_MY_TEMPLATE_ID
+const USER_ID = process.env.REACT_APP_MY_USER_ID
 
 const useForm = () => {
     
+    let formRef=useRef()
     const [name, setName] = useState('')
     const [message, setMessage] = useState('')
     const [email, setEmail] = useState('')
     const [submitAttempt, setSubmitAttempt] = useState(false)
-    
+  
     const formIsValid = name !== '' && message !== '' && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email.trim())
 
     const handleNameChange = (e) => {
@@ -26,6 +31,15 @@ const useForm = () => {
             console.log('not valid')
             return 
         }
+
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, formRef.current, USER_ID)
+        .then(result => {
+            console.log("SUCCESS? ", result.text)
+        }, (error) => {
+            console.log("Error? ", error.text)
+        })
+
+        setSubmitAttempt(false)
         setName('')
         setMessage('')
         setEmail('')
@@ -35,6 +49,7 @@ const useForm = () => {
         name,
         message,
         email,
+        formRef,
         handleNameChange,
         handleEmailChange,
         handleMessageChange,
