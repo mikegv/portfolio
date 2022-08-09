6791eea583f794374c0111c9cb10b9e93e351331
mikegv <xmik3yvx@live.com>
2022-08-09 14:55:38 -0700

    projects page responsiveness

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -15,7 +15,7 @@ const AboutMe = () => {
                 Hi, my name is Mike. I am a web developer who specializes in front end development. My main
                 tools are Javascript, CSS and React.  I am currently working as a React apprentice for Bitwise Industries under Alpha Works in
                 Fresno California. I am passionate about all things technology but especially love programming. In my free time
-                you can find me spending time outdoors with my family and sometimes watching the occasional 80's horror movie.
+                you can find me spending time outdoors with my family and sometimes watching the some 80's horror movies.
                 Feel free to contact me or check out my github using the links below.
             </p>
             
