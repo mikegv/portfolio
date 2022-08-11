8b1b0fab4ff0cd697f2cd8dc365b96ed9ecd3f51
mikegv <xmik3yvx@live.com>
2022-08-11 10:46:20 -0700

    resume image

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -1,38 +1,40 @@
-import React from 'react';
-import './AboutMe.css'
-import { motion } from 'framer-motion'
+import React from "react";
+import "./AboutMe.css";
+import { motion } from "framer-motion";
 import { Page, Document } from "react-pdf";
 
-
 const AboutMe = () => {
-    return (
-        <
-        motion.div 
-        initial={{scaleY: 0}} 
-        animate={{scaleY: 1}}
-        exit={{scaleY: 0}}
-        transition={{duration: .5}}
-        >
-            <p className='aboutMe'>
-                Hi, my name is Mike. I am a web developer who specializes in front end development. My main
-                tools are Javascript, CSS and React.  I am currently working as a React apprentice for Bitwise Industries under Alpha Works in
-                Fresno California. I am passionate about all things technology but especially love programming. In my free time
-                you can find me spending time outdoors with my family and sometimes watching some 80's horror movies.
-                Feel free to check out my resume or contact me using the links below.
-            </p>
-           
-                Resume:
-                <div className="resume-container">
-                <embed 
-                src={process.env.PUBLIC_URL + "/files/resume.pdf#view=FitH"} width="actual-width.px" height="actual-height.px"
-                type='application/pdf'
-                />
+  return (
+    <motion.div
+      initial={{ scaleY: 0 }}
+      animate={{ scaleY: 1 }}
+      exit={{ scaleY: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <p className="aboutMe">
+        Hi, my name is Mike. I am a web developer who specializes in front end
+        development. My main tools are Javascript, CSS and React. I am currently
+        working as a React apprentice for Bitwise Industries under Alpha Works
+        in Fresno California. I am passionate about all things technology but
+        especially love programming. In my free time you can find me spending
+        time outdoors with my family and sometimes watching some 80's horror
+        movies. Feel free to check out my resume or contact me using the links
+        below.
+      </p>
 
+      <div className="resume-container">
+        <embed
+          src={process.env.PUBLIC_URL + "/files/resume.pdf#view=FitH"}
+          type="application/pdf"
+          width="75%"
+          height="1100px"
+        />
       </div>
-           
-        </motion.div>
-
-    );
+      <div className="resume-image">
+        <img src={process.env.PUBLIC_URL + "/files/resume.JPG"} />
+      </div>
+    </motion.div>
+  );
 };
 
 export default AboutMe;
